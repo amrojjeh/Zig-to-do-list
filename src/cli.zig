b31144f27970d463b760c53b623b3dd772abc609
dinglydo <amrojjeh@gmail.com>
2021-04-23 02:16:15 -0500

    Added styles

--- src/cli.zig
+++ src/cli.zig
@@ -1,30 +1,6 @@
-const std = @import("std.zig");
-const Todo = @import("todo.zig");
+const Styles = struct {
+    pub const BOLD = "\x1B[1m";
+    pub const FAIL = "\x1B[91m";
+    pub const RESET = "\x1B[0m";
+};
 
-const Allocator = std.mem.Allocator;
-
-const TokenType = enum {
-    content, // End is marked by ;
-    start_of,
-    end_of,
-    next,
-    iso_date,
-
-    day,
-    week,
-    month,
-    year,
-}
-
-const Token = struct {
-    value: []const u8,
-    token_type: TokenType,
-}
-
-pub fn tokenize(alloc: *Allocator, buffer: []const u8) []Token {
-    const tokens = std.mem.tokenize(buffer, " ");
-    while (tokens.next()) |val| {
-        // {x} vs {X} for lowercase and uppwercase zig fmt
-    }
-
-}
