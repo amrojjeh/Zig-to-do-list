42984960a69116dc810179417388e48209df68d4
dinglydo <amrojjeh@gmail.com>
2021-05-10 02:40:21 -0500

    Moved cli_helper to cli

--- src/cli.zig
+++ src/cli.zig
@@ -1,5 +1,3 @@
-usingnamespace @import("cli_helper.zig");
-
 const std = @import("std");
 const io = @import("io.zig");
 const config = @import("config.zig");
@@ -179,3 +177,49 @@ fn clearAllTasks(alloc: *Allocator, args: *Arguments) !void {
     try io.save(todo);
     try printSuccess("👍 Deleted all tasks.", .{});
 }
+
+// ======= HELPER FUNCTIONS =======
+
+const Styles = struct {
+    pub const BOLD = "\x1B[1m";
+    pub const FAIL = "\x1B[91m" ++ BOLD;
+    pub const SUCCESS = "\x1B[32m" ++ BOLD;
+    pub const NORMAL = "\x1B[37m" ++ BOLD;
+
+    pub const STRIKE = "\x1B[9m";
+
+    pub const RESET = "\x1B[0m";
+};
+
+fn getWriter() std.fs.File.Writer {
+    return std.io.getStdOut().writer();
+}
+
+/// Prints a failed statement. Automatic newline.
+fn printFail(comptime str: []const u8, args: anytype) !void {
+    try print(Styles.FAIL, str, args);
+}
+
+fn printSuccess(comptime str: []const u8, args: anytype) !void {
+    try print(Styles.SUCCESS, str, args);
+}
+
+fn printNormal(comptime str: []const u8, args: anytype) !void {
+    try print(Styles.NORMAL, str, args);
+}
+
+fn print(style: []const u8, comptime str: []const u8, args: anytype) !void {
+    const writer = getWriter();
+    try writer.print("{s}", .{style});
+    try writer.print(str, args);
+    try writer.print("{s}\n", .{Styles.RESET});    
+}
+
+fn nextArgIndex(comptime T: type, args: *Arguments) !?T {
+    const str_num = args.next() orelse return 1;
+    return std.fmt.parseInt(T, str_num, 10) catch |err| blk: {
+        try printFail("{s} is not a number.", .{str_num});
+        break :blk null;
+    };
+}
+
