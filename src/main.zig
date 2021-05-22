f202451fe3bc21aedf5bb19f36deefb1e6a9fee3
dinglydo <amrojjeh@gmail.com>
2021-05-21 20:44:13 -0500

    Simplified function

--- src/main.zig
+++ src/main.zig
@@ -10,16 +10,13 @@ pub fn main() !void {
 
     const args = try std.process.argsAlloc(allocator);
 
-    var buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
-    config.FILE_NAME = getExeName(&buffer, args[0]);
+    config.FILE_NAME = getExeName(args[0]);
 
     try cli.execute(allocator, args[1..]);
 }
 
-fn getExeName(buffer: []u8, path: []const u8) []const u8 {
+fn getExeName(path: []const u8) []const u8 {
     var i: usize = path.len - 1;
-    while (i != 0 and path[i] != '\\') : (i -= 1) {
-        buffer[i] = path[i];
-    }
-    return buffer[i+1..path.len - 4]; // to remove the .exe
+    while (i != 0 and path[i] != '\\') : (i -= 1) { }
+    return path[i+1..path.len - 4]; // to remove the .exe
 }
